e322f8591dbbd7868fa1aea2bc9679de686d0e55
braginaliz <braginaliz@github.com>
2026-02-11 14:06:32 +0300

    Верстка (пп 3 блока)

--- wp-content/themes/kms-medicine-child/header.php
+++ wp-content/themes/kms-medicine-child/header.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php
+
+    $seo_title = get_field('seo_title');
+    $seo_description = get_field('seo_description');
+    
+    if (empty($seo_title)) {
+        $seo_title = 'Платная скорая помощь в Москве - врачи, услуги, цены';
+    }
+    if (empty($seo_description)) {
+        $seo_description = 'Платная скорая помощь в Москве и по всей России: вызов врача на дом, экстренная медицинская помощь, консультации специалистов. Узнайте актуальные цены, преимущества услуг и как заказать помощь 24/7.';
+    }
+    ?>
+    
+    <title><?php echo esc_html($seo_title); ?></title>
+    <meta name="description" content="<?php echo esc_attr($seo_description); ?>">
+    
     <link href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
